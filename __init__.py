0d7b91a83ef5b73c3423dae27c2d52453e188544
Marckie Zeender <marcus.zeender@nist.gov>
2021-06-07 10:36:42 -0600

    left panel collapses on resize to allow more space

--- __init__.py
+++ __init__.py
@@ -13,122 +13,123 @@ from PIL import Image, ImageTk
 import tkinter.font as font
 
 
-#Basic configuration
+# Basic configuration
 TITLE_ = 'MCV QoE'
-FONT_SIZE = 14
+FONT_SIZE = 13
+WIN_SIZE = (850, 700)
 
 # the initial values in all of the controls
 DEFAULT_CONFIG = {
     'EmptyFrame': {},
-    
-    'M2eFrame'  : {
-                'audio_file'      : "test.wav",
-                'bgnoise_file'    : "",
-                'bgnoise_volume'  : 0.1,
-                'blocksize'       : 512,
-                'buffersize'      : 20,
-                'fs'              : int(48e3),
-                'no_log'          : ['test', 'ri'],
-                'outdir'          : "",
-                'overplay'        : 1.0,
-                'ptt_wait'        : 0.68,
-                'radioport'       : "",
-                'test'            : "m2e_1loc",
-                'trials'          : 100
-            }
-    }
 
+    'M2eFrame': {
+        'audio_file': "test.wav",
+        'bgnoise_file': "",
+        'bgnoise_volume': 0.1,
+        'blocksize': 512,
+        'buffersize': 20,
+        'fs': int(48e3),
+        'no_log': ['test', 'ri'],
+        'outdir': "",
+        'overplay': 1.0,
+        'ptt_wait': 0.68,
+        'radioport': "",
+        'test': "m2e_1loc",
+                'trials': 100
+    }
+}
 
 
 class MCV_QoE_Gui(tk.Tk):
     """The main Gui
-    
+
 
     """
-    
-    
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
+
         set_font(size=FONT_SIZE)
-        
+
         self.title(TITLE_)
-        #dimensions
+        # dimensions
         self.minsize(width=600, height=370)
-        
-        
-        
-        
-        
-        #tk Variables to determine what test to run and show config for
+        self.geometry(f'{WIN_SIZE[0]}x{WIN_SIZE[1]}')
+
+        # tk Variables to determine what test to run and show config for
         self.is_simulation = tk.BooleanVar(value=False)
         self.selected_test = tk.StringVar(value='EmptyFrame')
-        
-        
+
         self.LeftFrame = LeftFrame(self, main_=self)
         self.LeftFrame.pack(side=tk.LEFT, fill=tk.Y)
-        
+
         self.bind('<Configure>', self.LeftFrame.on_change_size)
-        
-        
+
         self.frames = {}
-        #Initialize test-specific frames for the window
+        # Initialize test-specific frames for the window
         for F in (EmptyFrame, M2eFrame):
-            #loads the default values of the controls
+            # loads the default values of the controls
             svd = loadandsave.StringVarDict(**DEFAULT_CONFIG[F.__name__])
-            
-            #initializes the frame, with its key being its own classname
+
+            # initializes the frame, with its key being its own classname
             self.frames[F.__name__] = F(master=self, btnvars=svd,
                                         padx=10, pady=10)
-        
-        
+
         self.currentframe = self.frames['EmptyFrame']
         self.currentframe.pack()
-            
-        
 
     def show_frame(self, framename):
-        #hide the showing widget
+        # hide the showing widget
         self.currentframe.pack_forget()
-        
+
         self.currentframe = self.frames[framename]
         self.currentframe.pack(side=tk.RIGHT, fill=tk.BOTH, padx=10, pady=10)
 
-        
+    def is_empty(self):
+        return self.currentframe == self.frames['EmptyFrame']
+
+
 class LeftFrame(tk.Frame):
     """Can show and hide the MenuFrame using the MenuButton
-    
+
     Only applies when the main window is small enough
     """
+    # The window's minimum width at which the menu is shown
+    MenuShowWidth = 850
+
     def __init__(self, master, main_, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
-        
+        # stores the main Tk window
+        self.main_ = main_
+
         self.DoMenu = False
         self.MenuVisible = True
-        
+
         self.MenuButton = MenuButton(self, command=self.on_button)
-        self.MenuButton.pack()
-        
-        #pass main_ down the chain to the TestTypeFrame
+
+        # pass main_ down the chain to the TestTypeFrame
         self.MenuFrame = MenuFrame(self, main_)
         self.MenuFrame.pack(side=tk.LEFT, fill=tk.Y)
-        
+
     def on_change_size(self, event):
-        print(event.width)
+        w = self.MenuShowWidth
         if event.width < 600:
-            #potential case of invalid event
+            # potential case of invalid event
             return
-        if event.width <= 850 and not self.DoMenu:
+        if event.width < w and not self.DoMenu:
             self.DoMenu = True
             self.MenuVisible = False
-            self.MenuButton.show()
+            self.MenuButton.pack(side=tk.LEFT, fill=tk.Y)
             self.MenuFrame.pack_forget()
-        elif event.width > 850 and self.DoMenu:
+            if self.main_.is_empty():
+                self.MenuFrame.pack(side=tk.LEFT, fill=tk.Y)
+                self.MenuVisible = True
+        elif event.width >= w and self.DoMenu:
+            self.MenuButton.pack_forget()
             self.MenuVisible = True
             self.DoMenu = False
-            self.MenuButton.hide()
             self.MenuFrame.pack(side=tk.LEFT, fill=tk.Y)
-    
+
     def on_button(self):
         if self.MenuVisible:
             self.MenuVisible = False
@@ -136,93 +137,85 @@ class LeftFrame(tk.Frame):
         else:
             self.MenuVisible = True
             self.MenuFrame.pack(side=tk.LEFT, fill=tk.Y)
-        
+
 
 class MenuFrame(tk.Frame):
     """Contains the Logo frame and the Choose Test Type frame
-    
+
     """
+
     def __init__(self, master, main_, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
-        
+
         LogoFrame(master=self).pack(side=tk.LEFT, fill=tk.Y)
-        
-        #pass main_ down the chain to the TestTypeFrame
-        
-        
+
+        # pass main_ down the chain to the TestTypeFrame
+
         TestTypeFrame(master=self, main_=main_, padx=10, pady=10).pack(
             side=tk.LEFT, fill=tk.Y)
 
+
 class MenuButton(tk.Frame):
     def __init__(self, master, *args, command=None, **kwargs):
-        super().__init__(master, *args, **kwargs)
-        
-        self.btn = ttk.Button(self, text='menu', command=command) #image=Image.open('menu_icon.png'))
-    
-    def show(self):
-        self.btn.pack()
-        
-    def hide(self):
-        self.btn.pack_forget()
+        super().__init__(master, *args, **kwargs,)
 
+        #img = ImageTk.PhotoImage(Image.open('pscr_logo.png'))
+
+        tk.Button(master=self, text='...', command=command).pack()
 
 
 class TestTypeFrame(tk.Frame):
     """Allows the user to choose hardware/sim and which test to perform
-    
+
     """
+
     def __init__(self, master, main_, *args, **kwargs):
         super().__init__(master, *args, **kwargs)
         self.main_ = main_
-        
-        #StringVars to determine what test to run and show config for
-        is_sim  = main_.is_simulation
+
+        # StringVars to determine what test to run and show config for
+        is_sim = main_.is_simulation
         sel_txt = main_.selected_test
-        
-        
-        
+
         # Test Type
         ttk.Label(self, text='Choose Test Type:').pack(fill=tk.X)
-        
-        #hardware test
+
+        # hardware test
         ttk.Radiobutton(self, text='Hardware Test',
-            variable=is_sim, value=False).pack(fill=tk.X)
-        
-        #simulation test
+                        variable=is_sim, value=False).pack(fill=tk.X)
+
+        # simulation test
         ttk.Radiobutton(self, text='Simulation Test',
-            variable=is_sim, value=True).pack(fill=tk.X)
-                
-                
-        ttk.Separator(self).pack(fill=tk.X,pady=20)
-        
-        #Choose Test
+                        variable=is_sim, value=True).pack(fill=tk.X)
+
+        ttk.Separator(self).pack(fill=tk.X, pady=20)
+
+        # Choose Test
         ttk.Label(self, text='Choose Test:').pack(fill=tk.X)
-        
-        #Change test frame when user changes this option
+
+        # Change test frame when user changes this option
         cmd = self.change_frame
-        
+
         ttk.Radiobutton(self, text='M2E Latency', command=cmd,
-            variable=sel_txt, value='M2eFrame').pack(fill=tk.X)
-                
+                        variable=sel_txt, value='M2eFrame').pack(fill=tk.X)
+
         ttk.Radiobutton(self, text='Access Delay', command=cmd,
-            variable=sel_txt, value='AccssDFrame').pack(fill=tk.X)
-                
+                        variable=sel_txt, value='AccssDFrame').pack(fill=tk.X)
+
         ttk.Radiobutton(self, text='PSuD', command=cmd,
-            variable=sel_txt, value='PSuDFrame').pack(fill=tk.X)
-                
+                        variable=sel_txt, value='PSuDFrame').pack(fill=tk.X)
+
         ttk.Radiobutton(self, text='Intelligibility', command=cmd,
-            variable=sel_txt, value='IntgblFrame').pack(fill=tk.X)
-        
-                
+                        variable=sel_txt, value='IntgblFrame').pack(fill=tk.X)
+
     def change_frame(self):
-        self.main_.show_frame(self.master.selected_test.get())
-        
+        self.main_.show_frame(self.main_.selected_test.get())
+
 
 class LogoFrame(tk.Canvas):
-    
+
     crestinf = {'img': 'pscr_logo.png'}
-    
-    
+
     def __init__(self, master, width=150, height=170, *args, **kwargs):
         super().__init__(*args,
                          width=width,
@@ -230,43 +223,39 @@ class LogoFrame(tk.Canvas):
                          master=master,
                          bg='white',
                          **kwargs)
-        
+
         crest = self.crestinf['img']
         if crest != None:
-            
+
             img = Image.open(crest)
             img = img.resize(
                 (width, height),
                 Image.ANTIALIAS
-                )
+            )
             self.crestimg = ImageTk.PhotoImage(img)
             self.create_image(
                 width // 2, height // 2 + 10,
                 image=self.crestimg
-                )
-            
-            
-            
-
+            )
 
 
 class EmptyFrame(tk.Frame):
     """An empty frame: shown when no test is selected yet
-    
+
     """
+
     def __init__(self, btnvars, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        
+
+
 def set_font(**cfg):
     """Globally changes the font on all tkinter windows.
-    
+
     Accepts parameters like size, weight, font, etc.
 
     """
     font.nametofont('TkDefaultFont').config(**cfg)
 
 
-
 if __name__ == '__main__':
     wn = MCV_QoE_Gui()
-    
