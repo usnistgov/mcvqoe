28b344d877574d8e4ed2474b8aa77d46698cc451
Marckie Zeender <marcus.zeender@nist.gov>
2021-06-09 11:21:24 -0600

    migrated code to organization.py for use in other test frames

--- m2e_gui.py
+++ m2e_gui.py
@@ -8,10 +8,15 @@ Created on Wed Jun  2 08:52:09 2021
 import tkinter as tk
 import tkinter.ttk as ttk
 import tkinter.filedialog as fdl
+import organization
+from organization import LabeledControl
+
+PADX = organization.PADX
+PADY = organization.PADY
+
     
 
-PADX = 10
-PADY = 10
+
 
 
 class M2eFrame(tk.LabelFrame):
@@ -61,7 +66,7 @@ class M2EAdvancedConfigGUI(tk.Toplevel):
         super().__init__(*args, **kwargs)
         
         
-        self.title('Advanced')
+        self.title('M2E Latency - Advanced')
         #sets the controls in this window
         controls = (
             BgNoise,
@@ -92,76 +97,6 @@ class M2EAdvancedConfigGUI(tk.Toplevel):
         
         
         #------------------ Controls ----------------------
-class LabeledControl():
-    """A one-row grid consisting of a label, control, and optional 2nd control
-    
-    row : int
-        the row that the controls should be gridded in
-    
-    """
-    text = ''
-    
-    
-    MCtrl = ttk.Entry
-    MCtrlargs = []
-    MCtrlkwargs = {}
-    
-    variable_arg = 'textvariable'
-    
-    #usually the browse button
-    RCtrl = None
-    RCtrlkwargs = {}
-    
-    padx = PADX
-    pady = PADY
-    
-    def __init__(self, master, row):
-        self.master = master
-        
-    
-        ttk.Label(master, text=self.text).grid(
-            padx=self.padx, pady=self.pady, column=0, row=row, sticky='E')
-        
-        MCtrlkwargs = self.MCtrlkwargs.copy()
-        MCtrlargs = self.MCtrlargs.copy()
-        RCtrlkwargs = self.RCtrlkwargs.copy()
-        
-        try:
-            self.btnvar = master.btnvars[self.__class__.__name__]
-        except KeyError:
-            self.btnvar = None
-            
-            
-        # some controls require the textvariable=... to be positional
-        
-        #some controls require more flexibility, so they don't use self.MCtrl
-        if self.MCtrl:
-            if self.variable_arg:
-                MCtrlkwargs[self.variable_arg] = self.btnvar
-            
-            else:
-                MCtrlargs.insert(0, self.btnvar)
-                
-            
-            # initialize the control
-            self.MCtrl(master, *MCtrlargs, **MCtrlkwargs).grid(
-                column=1, row=row, padx=self.padx, pady=self.pady, sticky='WE')
-        
-        
-        if self.RCtrl:
-            #add command to button
-            if self.RCtrl in (ttk.Button, tk.Button):
-                RCtrlkwargs['command'] = self.on_button
-            
-            # initialize the control
-            self.RCtrl(master, **RCtrlkwargs).grid(
-                padx=self.padx, pady=self.pady, column=2, row=row, sticky='WE')
-            
-            
-            
-    def on_button(self):
-        pass
-            
 
 
 
