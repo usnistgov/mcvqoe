946fb4e2571f8efa320ca10672feeaa546c23720
Marckie Zeender <marcus.zeender@nist.gov>
2021-06-11 15:19:09 -0600

    migrated content to shared.py for reuse in access dly

--- m2e_gui.py
+++ m2e_gui.py
@@ -12,32 +12,16 @@ import tkinter as tk
 import tkinter.ttk as ttk
 import tkinter.filedialog as fdl
 
-from shared import LabeledControl
-from shared import PADX
-from shared import PADY
-from shared import _SignalOverride
-from tkinter.scrolledtext import ScrolledText
+from shared import PADX, PADY, TestCfgFrame, _SignalOverride, LabeledControl
 import shared
-    
-import time
-import sys
 
 
-
-class M2eFrame(ttk.LabelFrame):
-    """
-    Gui to configure and run a M2E Latency Test
+class M2eFrame(TestCfgFrame):
     
-    strvars : StringVarDict
-    loads and stores the values in the controls
+    text = 'Mouth-to-Ear Latency Test'
     
-    """
-       
-    
-    def __init__(self, btnvars, text='Mouth-to-Ear Latency Test',
-                 *args, **kwargs):
-        #sets what controls will be in this frame
-        controls = (
+    def get_controls(self):
+        return (
             test,
             audio_files,
             trials,
@@ -45,61 +29,8 @@ class M2eFrame(ttk.LabelFrame):
             overplay,
             outdir,
             advanced
+            
             )
-        
-        
-        
-        super().__init__(*args, text=text, **kwargs)
-        #option functions will get and store their values in here
-        self.btnvars = btnvars
-        
-        
-        #initializes controls
-        for row in range(len(controls)):
-            controls[row](master=self, row=row)
-        
-        
-        
-            
-            
-class M2EAdvancedConfigGUI(tk.Toplevel):
-    """Advanced options for the M2E test
-    
-
-    """    
-    
-    def __init__(self, btnvars, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        
-        
-        self.title('M2E Latency - Advanced')
-        #sets the controls in this window
-        controls = (
-            BgNoise,
-            AudioSettings,
-            radioport,
-            _advanced_submit
-            )
-        
-        self.btnvars = btnvars
-        
-        #Sets window on top of other windows
-        self.attributes('-topmost', True)
-        
-        #initializes controls
-        for row in range(len(controls)):
-            controls[row](master=self, row=row)
-        
-        
-        # return key closes window
-        self.bind('<Return>', lambda *args : self.destroy())
-        
-        #sets focus on the window
-        self.focus_force()
-        
-            
-        
-        
         
         
         #------------------ Controls ----------------------
@@ -130,22 +61,7 @@ class test(LabeledControl):
     MCtrl = _test
     
 
-class audio_files(LabeledControl):
-    text = 'Audio File(s):'
-    RCtrl = ttk.Button
-    RCtrlkwargs = {
-        'text' : 'Browse...'
-        }
-    
-    def on_button(self):
-        fp = fdl.askopenfilenames(parent=self.master,
-                initialfile=self.btnvar.get(),
-                filetypes=[('WAV files', '*.wav')])
-        if fp:
-            str_ = ', '
-            self.btnvar.set(str_.join(fp))
-            
-            
+from shared import audio_files
             
             
 class audio_file(LabeledControl):
@@ -165,167 +81,12 @@ class audio_file(LabeledControl):
         }
     
     
-class trials(LabeledControl):
-    text = 'Number of Trials:'
-    MCtrl = ttk.Spinbox
-    MCtrlkwargs = {'from_' : 1, 'to' : 2**15 - 1}
-    
+from shared import trials, ptt_wait, outdir, advanced
 
-class radioport(LabeledControl):
-    text = 'Radio Port:'
-    
-    
-    
-class bgnoise_file(LabeledControl):
-    text = 'Noise File:'
-    
-    RCtrl = ttk.Button
-    RCtrlkwargs = {'text' : 'Browse...'}
-    
-    def on_button(self):
-        fp = fdl.askopenfilename(parent=self.master,
-            initialfile=self.btnvar.get(),
-            filetypes=[('WAV files', '*.wav')])
-        if fp:
-            self.btnvar.set(fp)
-
-
-class bgnoise_volume(LabeledControl):
-    text = 'Volume:'
-    RCtrl = None
-    MCtrl = None
-    # slider does not accept font size
-    do_font_scaling = False    
-    
-    
-    def __init__(self, master, row, *args, **kwargs):
-        super().__init__(master, row, *args, **kwargs)
-        self.txtvar = _bgnoise_volume_percentage()
-        self.on_change()
-        
-        self.btnvar.trace_add('write', self.on_change)
-    
-        ttk.Label(master, textvariable=self.txtvar).grid(
-            column=2, row=row, sticky='W')
-    
-        ttk.Scale(master, variable=self.btnvar,
-            from_=0, to=1).grid(
-            column=1, row=row, padx=self.padx, pady=self.pady, sticky='WE')
-    
-    def on_change(self, *args, **kwargs):
-        #updates the percentage to match the value of the slider
-        self.txtvar.set(self.btnvar.get())
-
-class _bgnoise_volume_percentage(tk.StringVar):
-    """Displays a percentage instead of a float
-    
-    """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        
-    def set(self, value):
-        v = float(value) * 100
-        s = ''
-        
-        for char in str(v):
-            if char == '.':
-                break
-            s = f'{s}{char}'
-        
-                    
-        super().set(f'{s}%')
-            
-        
-class ptt_wait(LabeledControl):
-    text = 'PTT Wait Time (sec):'
-    
-    MCtrl = ttk.Spinbox
-    MCtrlkwargs = {'increment' : 0.01, 'from_' : 0, 'to' : 2**15 - 1}
-
-              
-                                  
-class blocksize(LabeledControl):
-    text = 'Block Size:'
-    
-
-class buffersize(LabeledControl):
-    text='Buffer Size:'
-    
 class overplay(LabeledControl):
     text='Overplay Time (sec):'
     MCtrl = ttk.Spinbox
     MCtrlkwargs = {'increment':0.01, 'from_':0, 'to':2**15 -1}
-
-class outdir(LabeledControl):
-    text='Output Folder:'
-    
-    RCtrl = ttk.Button
-    RCtrlkwargs = {'text': 'Browse...'}
-    
-    def on_button(self):
-        dirp = fdl.askdirectory(parent=self.master)
-        if dirp:
-            self.btnvar.set(dirp)
-            
-            
-    def __init__(self, *args, **kwargs):
-        
-        super().__init__(*args, **kwargs)
-        
-        
-
-class advanced(LabeledControl):
-    text = ''
-    
-    MCtrl = None
-    RCtrl = ttk.Button       
-    RCtrlkwargs = {'text': 'Advanced...'}
-    
-    def on_button(self):
-        M2EAdvancedConfigGUI(btnvars=self.master.btnvars)
-
-    
-
-class _advanced_submit(LabeledControl):
-    
-    #closes the advanced window
-    MCtrl = None
-    RCtrl = ttk.Button
-    RCtrlkwargs = {'text': 'OK'}
-    
-    def on_button(self):
-        self.master.destroy()
-    
-    
-# advanced groups
-class BgNoise(ttk.LabelFrame):
-    def __init__(self, master, row, *args, **kwargs):
-        super().__init__(master, *args, text='Background Noise', **kwargs)
-        
-        self.btnvars = master.btnvars
-        
-        controls = (bgnoise_file, bgnoise_volume)
-        
-        for row_ in range(len(controls)):
-            controls[row_](master=self, row=row_)
-        
-        self.grid(column=0, row=row, padx=PADX, pady=PADY, columnspan=3,
-                  sticky='WE')
-        
-
-class AudioSettings(ttk.LabelFrame):
-    def __init__(self, master, row, *args, **kwargs):
-        super().__init__(master, *args, text='Audio Settings', **kwargs)
-        
-        self.btnvars = master.btnvars
-        
-        controls = (blocksize, buffersize)
-        
-        for row_ in range(len(controls)):
-            controls[row_](master=self, row=row_)
-        
-        self.grid(column=0, row=row, padx=PADX, pady=PADY, columnspan=3,
-                  sticky='WE')
 
 
 
@@ -338,10 +99,7 @@ class AudioSettings(ttk.LabelFrame):
 
 
 def run(cnf, is_simulation):
-    print(cnf,'\n', is_simulation)
     
-     
-
      
     o = m2e_class.M2E()
     
@@ -372,10 +130,8 @@ def run(cnf, is_simulation):
         m2e_class.RadioInterface = get_sim
         m2e_class.AudioPlayer = get_sim
     
-    print('now')
     # Open RadioInterface object for testing
     o.ri = m2e_class.RadioInterface(o.radioport)
-    print('you')
     # Fill 'Arguments' within info dictionary
     o.info.update(m2e_class.write_log.fill_log(o))
 
@@ -389,7 +145,6 @@ def run(cnf, is_simulation):
     # Write pretest notes and info to tests.log
     m2e_class.write_log.pre(info=o.info)
     
-    print('are')
     # Run chosen M2E test
     if (o.test == "m2e_1loc"):
         o.m2e_1loc()
@@ -405,7 +160,6 @@ def run(cnf, is_simulation):
     m2e_class.write_log.post(info=post_dict, outdir=o.outdir)
     
     
-    print('done')
      
      
      
@@ -413,12 +167,24 @@ def run(cnf, is_simulation):
      
      
 class _QoEsim_Override(QoEsim):
+    """ONLY TEMPORARY: makes QoEsim accept filename instead of 
+    
+    """
     def play_record(*args, **kwargs):
         if 'filename' in kwargs:
             kwargs['out_name'] = kwargs['filename']
             del kwargs['filename']
         super().play_record(*args, **kwargs)
      
-     
-     
-     
+
+from shared import BgNoise, AudioSettings, radioport
+
+class M2EAdvancedConfigGUI(shared.AdvancedConfigGUI):
+    
+    
+    def get_controls(self):
+        return (
+            BgNoise,
+            AudioSettings,
+            radioport,
+            )
--- shared.py
+++ shared.py
@@ -7,14 +7,90 @@ Created on Wed Jun  9 11:03:39 2021
 
 import tkinter as tk
 from tkinter import ttk
-import tkinter.font as tkFont
-
-import signal
+import tkinter.filedialog as fdl
 
 PADX = 10
 PADY = 10
 
 FONT_SIZE = 11
+
+class TestCfgFrame(ttk.LabelFrame):
+    """
+    Base class to configure and run a  Test
+    
+    btnvars : StringVarDict
+    loads and stores the values in the controls
+    
+    """
+    
+    text = ''
+       
+    
+    def __init__(self, btnvars,
+                 *args, **kwargs):
+        kwargs['text'] = self.text
+        super().__init__(*args, **kwargs)
+        #option functions will get and store their values in here
+        self.btnvars = btnvars
+        
+        
+        #sets what controls will be in this frame
+        controls = self.get_controls()
+        
+        #initializes controls
+        for row in range(len(controls)):
+            controls[row](master=self, row=row)
+        
+        
+        
+    def get_controls() -> iter:
+        """subclasses should override this
+        
+
+
+        """
+
+
+
+class AdvancedConfigGUI(tk.Toplevel):
+    """Advanced options for the M2E test
+    
+
+    """    
+    
+    def __init__(self, btnvars, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        
+        self.title('M2E Latency - Advanced')
+        #sets the controls in this window
+        controls = list(self.get_controls())
+        controls.append(_advanced_submit)
+        
+        self.btnvars = btnvars
+        
+        #Sets window on top of other windows
+        self.attributes('-topmost', True)
+        
+        #initializes controls
+        for row in range(len(controls)):
+            controls[row](master=self, row=row)
+            
+        
+        
+        
+        # return key closes window
+        self.bind('<Return>', lambda *args : self.destroy())
+        
+        #sets focus on the window
+        self.focus_force()
+        
+            
+    def get_controls():
+        pass
+    
+    
+    
 
 class LabeledControl():
     """A one-row grid consisting of a label, control, and optional 2nd control
@@ -101,6 +177,190 @@ class LabeledControl():
     def on_button(self):
         pass
             
+
+
+#controls
+class audio_files(LabeledControl):
+    text = 'Audio File(s):'
+    RCtrl = ttk.Button
+    RCtrlkwargs = {
+        'text' : 'Browse...'
+        }
+    
+    def on_button(self):
+        fp = fdl.askopenfilenames(parent=self.master,
+                initialfile=self.btnvar.get(),
+                filetypes=[('WAV files', '*.wav')])
+        if fp:
+            str_ = ', '
+            self.btnvar.set(str_.join(fp))
+            
+            
+class trials(LabeledControl):
+    text = 'Number of Trials:'
+    MCtrl = ttk.Spinbox
+    MCtrlkwargs = {'from_' : 1, 'to' : 2**15 - 1}
+    
+class outdir(LabeledControl):
+    text='Output Folder:'
+    
+    RCtrl = ttk.Button
+    RCtrlkwargs = {'text': 'Browse...'}
+    
+    def on_button(self):
+        dirp = fdl.askdirectory(parent=self.master)
+        if dirp:
+            self.btnvar.set(dirp)
+            
+            
+    def __init__(self, *args, **kwargs):
+        
+        super().__init__(*args, **kwargs)
+        
+        
+
+
+class radioport(LabeledControl):
+    text = 'Radio Port:'
+    
+    
+class bgnoise_file(LabeledControl):
+    text = 'Noise File:'
+    
+    RCtrl = ttk.Button
+    RCtrlkwargs = {'text' : 'Browse...'}
+    
+    def on_button(self):
+        fp = fdl.askopenfilename(parent=self.master,
+            initialfile=self.btnvar.get(),
+            filetypes=[('WAV files', '*.wav')])
+        if fp:
+            self.btnvar.set(fp)
+
+
+class bgnoise_volume(LabeledControl):
+    text = 'Volume:'
+    RCtrl = None
+    MCtrl = None
+    # slider does not accept font size
+    do_font_scaling = False    
+    
+    
+    def __init__(self, master, row, *args, **kwargs):
+        super().__init__(master, row, *args, **kwargs)
+        self.txtvar = _bgnoise_volume_percentage()
+        self.on_change()
+        
+        self.btnvar.trace_add('write', self.on_change)
+    
+        ttk.Label(master, textvariable=self.txtvar).grid(
+            column=2, row=row, sticky='W')
+    
+        ttk.Scale(master, variable=self.btnvar,
+            from_=0, to=1).grid(
+            column=1, row=row, padx=self.padx, pady=self.pady, sticky='WE')
+    
+    def on_change(self, *args, **kwargs):
+        #updates the percentage to match the value of the slider
+        self.txtvar.set(self.btnvar.get())
+
+class _bgnoise_volume_percentage(tk.StringVar):
+    """Displays a percentage instead of a float
+    
+    """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+    def set(self, value):
+        v = float(value) * 100
+        s = ''
+        
+        for char in str(v):
+            if char == '.':
+                break
+            s = f'{s}{char}'
+        
+                    
+        super().set(f'{s}%')
+            
+        
+class ptt_wait(LabeledControl):
+    text = 'PTT Wait Time (sec):'
+    
+    MCtrl = ttk.Spinbox
+    MCtrlkwargs = {'increment' : 0.01, 'from_' : 0, 'to' : 2**15 - 1}
+
+              
+                                  
+class blocksize(LabeledControl):
+    text = 'Block Size:'
+    
+
+class buffersize(LabeledControl):
+    text='Buffer Size:'
+    
+
+
+
+
+
+class advanced(LabeledControl):
+    text = ''
+    
+    MCtrl = None
+    RCtrl = ttk.Button       
+    RCtrlkwargs = {'text': 'Advanced...'}
+    
+    def on_button(self):
+        M2EAdvancedConfigGUI(btnvars=self.master.btnvars)
+
+    
+
+class _advanced_submit(LabeledControl):
+    
+    #closes the advanced window
+    MCtrl = None
+    RCtrl = ttk.Button
+    RCtrlkwargs = {'text': 'OK'}
+    
+    def on_button(self):
+        self.master.destroy()
+    
+    
+# advanced groups
+class BgNoise(ttk.LabelFrame):
+    def __init__(self, master, row, *args, **kwargs):
+        super().__init__(master, *args, text='Background Noise', **kwargs)
+        
+        self.btnvars = master.btnvars
+        
+        controls = (bgnoise_file, bgnoise_volume)
+        
+        for row_ in range(len(controls)):
+            controls[row_](master=self, row=row_)
+        
+        self.grid(column=0, row=row, padx=PADX, pady=PADY, columnspan=3,
+                  sticky='WE')
+        
+
+class AudioSettings(ttk.LabelFrame):
+    def __init__(self, master, row, *args, **kwargs):
+        super().__init__(master, *args, text='Audio Settings', **kwargs)
+        
+        self.btnvars = master.btnvars
+        
+        controls = (blocksize, buffersize)
+        
+        for row_ in range(len(controls)):
+            controls[row_](master=self, row=row_)
+        
+        self.grid(column=0, row=row, padx=PADX, pady=PADY, columnspan=3,
+                  sticky='WE')
+
+
+
+
+
 
 
 
