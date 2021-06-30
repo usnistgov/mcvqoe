0d898d86414a211af182d51cc6cf3370f0cdd1da
Marckie Zeender <marcus.zeender@nist.gov>
2021-06-30 13:51:53 -0700

    overrided plot() method to show in main gui

--- m2e_gui.py
+++ m2e_gui.py
@@ -15,6 +15,11 @@ from shared import TestCfgFrame, LabeledControl
 import shared
 
 
+import csv
+from matplotlib.figure import Figure
+import numpy as np
+
+
 class M2eFrame(TestCfgFrame):
     
     text = 'Mouth-to-Ear Latency Test'
@@ -128,7 +133,64 @@ class M2E_fromGui(shared.SignalOverride, m2e.measure):
             raise ValueError("\nIncorrect test type")
             
         
-    
-    
+    def plot(self,name=None):
+        
+        if( not name):
+            name=self.data_filename
+        
+        with open(name,'rt') as csv_f:
+            #create dict reader
+            reader=csv.DictReader(csv_f)
+            #empty list for M2E data
+            m2e_dat=[]
+            #
+            for row in reader:
+                m2e_dat.append(float(row['m2e_latency']))
+
+        #convert to numpy array
+        m2e_dat=np.array(m2e_dat)
+        
+        #----------------------------[Generate Plots]------------------------------
+        
+        # Overall mean delay
+        ovrl_dly = np.mean(m2e_dat)
+        
+        # Get standard deviation
+        std_delay = np.std(m2e_dat, dtype=np.float64)
+        std_delay = std_delay*(1e6)
+        
+        # Print StD to terminal
+        
+        std_msg = "StD: %.2fus" % std_delay
+        
+        print(std_msg, flush=True)
+        
+        
+        # Create trial scatter plot
+        fig1 = Figure()
+        a = fig1.add_subplot()
+        
+        x2 = range(1, len(m2e_dat)+1)
+        a.scatter(x2,m2e_dat, color='blue')
+        a.set_xlabel("Trial Number")
+        a.set_ylabel("Delay(s)")
+        
+        
+        # Create histogram for mean
+        fig2 = Figure()
+        a = fig2.add_subplot()
+        uniq = np.unique(m2e_dat)
+        dlymin = np.amin(m2e_dat)
+        dlymax = np.amax(m2e_dat)
+        a.hist(m2e_dat, bins=len(uniq), range=(dlymin, dlymax), rwidth=0.5)
+        fig2.suptitle("Mean: %.5fs" % ovrl_dly)
+        a.set_xlabel("Delay(s)")
+        a.set_ylabel("Frequency of indicated delay")
+        
+        
+        #show this info in window
+        self.gui_show_element(std_msg)
+        self.gui_show_element(fig1)
+        self.gui_show_element(fig2)
 
 
