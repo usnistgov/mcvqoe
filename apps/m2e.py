c3a855dbe81f19a7daeb9e36c82a13123f87a9d5
Jaden <jaden.pieper@nist.gov>
2021-10-14 09:02:09 -0600

    Messing with upload

--- apps/m2e.py
+++ apps/m2e.py
@@ -10,13 +10,19 @@ from dash.dependencies import Input, Output
 from ipywidgets import widgets
 from plotly.subplots import make_subplots
 
+
+import base64
 import dash
+
+import datetime
+import io
 
 from app import app
 
 import mcvqoe.mouth2ear as mouth2ear
 import numpy as np
 import os
+import pandas as pd
 import plotly.graph_objects as go
 
 # test_data = 'D:\MCV_671DRDOG\mouth2ear-internal\data\csv\capture_QoE board baseline_21-Sep-2021_15-09-04.csv'
@@ -47,6 +53,26 @@ layout = html.Div([
     html.H1('MCV QoE Measurements Processing'),
     
     html.H3('Mouth-to-ear latency data analysis'),
+    
+    dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'Drag and drop or ',
+            html.A('Select Files')
+            ]),
+        style={
+            'width': '50%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+            },
+        multiple=True
+        ),
+    html.Div(id='output-data-upload'),
     
     html.H4(f'Evaluating session(s): {m2e_eval.test_names}'),
     html.P(f'Mean mouth-to-ear latency: {m2e_mean} s'),
@@ -119,22 +145,51 @@ layout = html.Div([
     ])
 
 
-# @app.callback(
-#     Output('plots', 'figure'),
-#     Input('thin-select', 'value')
-#     )
-# def update_plots(thin):
-#     # fig = make_subplots(
-#     #               rows=1, cols=2,
-#     #               subplot_titles=("Scatter", "Histogram")
-#     #               )
-#     print('in update_plots')
-#     thinned = thin == 'True'
-#     scatter = m2e_eval.plot(thinned=thinned)
-#     histogram = m2e_eval.histogram(thinned=thinned)
-#     fig = widgets.HBox([scatter, histogram])
-#     fig.show()
-#     return fig
+def parse_contents(contents, filename, date):
+    content_type, content_string = contents.split(',')
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'csv' in filename:
+            df = pd.read_csv(
+                io.StringIO(decoded.decode('utf-8'))
+                )
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file'
+            ])
+    return html.Div([
+            html.H5(filename),
+            html.H6(datetime.datetime.fromtimestamp(date)),
+            
+            dash.dash_table.DataTable(
+                data=df.to_dict('records'),
+                columns=[{'name': i, 'id': i} for i in df.columns],
+                page_size=10,
+                ),
+            html.Hr(),
+            
+            # For debugging, display the raw contents provided by the web browser
+            html.Div('Raw Content'),
+            html.Pre(contents[0:200] + '...', style={
+                'whiteSpace': 'pre-wrap',
+                'wordBreak': 'break-all'
+            })
+        ])
+
+@app.callback(
+    Output('output-data-upload', 'children'),
+    Input('upload-data', 'contents'),
+    Input('upload-data', 'filename'),
+    Input('upload-data', 'last_modified'),
+    )
+def update_output(list_of_contents, list_of_names, list_of_dates):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+    return children
+
 @app.callback(
     Output('m2e_scatter', 'figure'),
     Input('thin-select', 'value'),
