b3db164563697d2efa2424511036f0f8f351aed4
Jaden <jaden.pieper@nist.gov>
2021-10-27 17:56:02 -0600

    Cleaned up psud page a bit

--- mcvqoe/hub/eval_psud.py
+++ mcvqoe/hub/eval_psud.py
@@ -1,35 +1,31 @@
 # -*- coding: utf-8 -*-
 """
-Created on Tue Oct  5 14:51:35 2021
+Created on Tue Oct 12 12:26:08 2021
 
 @author: jkp4
 """
 from dash import dcc
 from dash import html
-import dash
+from dash.dependencies import Input, Output, State
+
+import base64
+import io
+import json
+import numpy as np
+import os
+import pandas as pd
+import tempfile 
 
 from mcvqoe.hub.eval_app import app
-# from .eval_app import app
 
-layout = html.Div([
-    html.H1('MCV QoE Measurements Processing'),
-    
-    html.H3('PSuD Page'),
-    
-    dcc.Dropdown(
-        id='psud-dropdown',
-        options=[
-            {'label': f'{i}', 'value': i} for i in [
-                'What', 'am', 'I', 'doing'
-                ]
-            ]
-        ),
-    html.Div(id='psud-display'),
-    dcc.Link('Go to Data Loading', href='/apps/measurement_select')
-    ])
+import mcvqoe.hub.eval_shared as eval_shared
+import mcvqoe.psud as psud
 
-@app.callback(
-    dash.dependencies.Output('psud-display', 'children'),
-    dash.dependencies.Input('psud-dropdown', 'value'))
-def display_psud_value(value):
-    return f'You have selected {value}'
+
+
+#-----------------------[Begin layout]---------------------------
+# TODO: Say something about common thinning fctor if data can't be thined
+
+
+layout = eval_shared.layout_template('psud')
+
