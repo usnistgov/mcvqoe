44ec5f21da9047ba0c628c54dcf3bbee327c669b
cjh12 <charlsea.hansen@nist.gov>
2023-10-27 15:48:15 -0400

    updating Image.ANTIALIAS as it is deprecated as of Pillow v10

--- mcvqoe/hub/mcv_qoe_gui.py
+++ mcvqoe/hub/mcv_qoe_gui.py
@@ -1934,7 +1934,7 @@ class LogoFrame(tk.Canvas):
                 img = Image.open(crest)
                 img = img.resize(
                     (width, height),
-                    Image.ANTIALIAS
+                    Image.Resampling.LANCZOS
                 )
                 self.crestimg = ImageTk.PhotoImage(img)
                 self.create_image(
